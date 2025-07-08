bf567300a31f08f9faf8b71fdf378b543836ac13
RyanTorresmo <nerdryantorres@gmail.com>
2025-07-08 09:09:03 -0300

    ultimo

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into tbl_contatos
+(nome,
+ sobrenome,
+ email,
+ telefone,
+ celular,
+ endereco,
+ cidade,
+ estado,
+ categoria)
+ values('Henrique', 'garcia', 'henrique.garcia@gmail.com', '1199999999', 'iphone', 'rua jasmin', 'porto feliz', 'sao paulo', 'faculdade');
